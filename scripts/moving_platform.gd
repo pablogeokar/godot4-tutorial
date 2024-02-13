37d214a6c9164fe37131195812db2ebd03f1de4c
Pablo George <pablogeokar@hotmail.com>
2024-02-13 14:05:55 -0300

    criando, plataformas, inimigos e animação das nuvens

--- scripts/moving_platform.gd
+++ scripts/moving_platform.gd
@@ -16,7 +16,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
 	platform.position = platform.position.lerp(follow, 0.5)
 
 func move_platform():
